8e07ab69a66063fd86701e485ad6de921de3819e
saintdi25u <corentinstd@gmail.com>
2023-03-23 10:56:12 +0100

    Commit initial et création du projet

---
+++ applastfm/src/main/java/fr/ul/miage/structu/applastfm/App.java
@@ -0,0 +1,14 @@
+package fr.ul.miage.structu.applastfm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    // API-KEY : dcfdc8018e4fab59a9015539adc6b50e
+}
---
+++ applastfm/src/main/java/fr/ul/miage/structu/applastfm/HTTPTools.java
@@ -0,0 +1,57 @@
+package fr.ul.miage.structu.applastfm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Outils pour HTTP
+ *
+ */
+public class HTTPTools {
+    // temps minimum d'une requête HTTP en ms (4 seconds)
+    private int mt = 4000;
+
+    // dernière requête HTTP
+    private long last;
+
+    /**
+     * Envoi une requête GET
+     * 
+     * @param URL de la requête
+     * @return reponse
+     */
+    public String sendGet(String url) {
+        // vérifie le temps écoulé depuis la requête précédente
+        while (System.currentTimeMillis() - last < mt)
+            ;
+        last = System.currentTimeMillis();
+        try {
+            // préparation de la requête
+            StringBuilder result = new StringBuilder();
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("GET");
+            InputStreamReader isr = new InputStreamReader(conn.getInputStream(), "UTF8");
+            BufferedReader br = new BufferedReader(isr);
+
+            // obtention de la réponse
+            String line;
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+            // fermeture du lecteur et retour
+            br.close();
+            isr.close();
+            return result.toString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
