a33b1a0350173bc7b755913a0a570ec9ca2be8d4
Theo <manuellitheo@gmail.com>
2023-04-19 22:15:47 +0200

    push avant merge

--- applastfm/src/main/java/fr/ul/miage/structu/applastfm/Main.java
+++ applastfm/src/main/java/fr/ul/miage/structu/applastfm/Main.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         RequestManager rm = new RequestManager();
-        rm.Top10Global("France") ;    
+        rm.getTrack("Business","Eminem") ;
+        //rm.getAlbumMusicInfo("Enna","PLK");
+        //rm.getInfoArtiste("Nekfeu");
+        
+        // typeDeRecherche doit être égale à Tracks, album ou artist
+        //rm.ecouteSup(8000000, "artist", "inferieur");
     }
 }
